3d4d586bc6e492eb6fd1a273ab2ce6aec83c24ac
Antoshka <tosh234@mail.ru>
2019-04-12 01:19:47 +0300

    Add Laba_6

---
+++ Laba_6/Laba_6/Cat.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::drink()
+{
+	cout << "Пить молоко!" << endl;
+}
+void Cat::play()
+{
+	cout << "Играть с хозяином!" << endl;
+}
